6f4c48081975ebd88ec7799286a4c2fee3160127
LogWat <s2420668@u.tsukuba.ac.jp>
2025-05-25 16:52:40 +0900

    dev&fix: livox imu scale考慮するように

--- src/imu_transform_component.cpp
+++ src/imu_transform_component.cpp
@@ -19,6 +19,8 @@ public:
                 output_ = this->get_parameter("output").as_string();
             this->declare_parameter("output_frame", "base_link");
                 output_frame_ = this->get_parameter("output_frame").as_string();
+            this->declare_parameter("is_livox_imu", true);
+                is_livox_imu_ = this->get_parameter("is_livox_imu").as_bool();
             
             // transform (tx, ty, tz, roll, pitch, yaw)
             this->declare_parameter("transform", std::vector<double>{0, 0, 0, 0, 0, 0});
@@ -28,10 +30,10 @@ public:
                 return;
             }
                 Eigen::Vector3d t(transform[0], transform[1], transform[2]);
-                tf2::Quaternion q;
-                q.setRPY(transform[3], transform[4], transform[5]);
-                Eigen::Matrix3d R;
-                tf2::Matrix3x3(q).getRPY(R(0, 0), R(1, 1), R(2, 2));
+                tf2::Quaternion q_tf;
+                q_tf.setRPY(transform[3], transform[4], transform[5]);
+                Eigen::Quaterniond q_eig(q_tf.getW(), q_tf.getX(), q_tf.getY(), q_tf.getZ());
+                Eigen::Matrix3d R = q_eig.toRotationMatrix();
                 imu_transformer_ = std::make_unique<IMUTransformer>(R, t);
             RCLCPP_INFO(this->get_logger(), "Transformation - Translation: [%f, %f, %f], Rotation: [%f, %f, %f]",
                 transform[0], transform[1], transform[2], transform[3], transform[4], transform[5]);
@@ -43,31 +45,49 @@ public:
             );
             imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(output_, rclcpp::SensorDataQoS());
 
+            prev_sensor_data_time_ = 0.0;
+            prev_imu_gyro_ = Eigen::Vector3d(0.0, 0.0, 0.0);
         }
     IMUTransformComponent(const rclcpp::NodeOptions& options, const std::string & node_name);
 
 private:
     void imuCallback(const sensor_msgs::msg::Imu::ConstSharedPtr &imu_raw) {
         double sensor_now = imu_raw->header.stamp.sec + imu_raw->header.stamp.nanosec * 1e-9;
-        if (prev_sensor_data_time_ == 0.0 || prev_imu_gyro_.norm() == 0.0) {
-            prev_imu_gyro_ = Eigen::Vector3d(imu_raw->angular_velocity.x, imu_raw->angular_velocity.y, imu_raw->angular_velocity.z);
-            prev_sensor_data_time_ = sensor_now;
-            return;
-        }
-        double dt = sensor_now - prev_sensor_data_time_;
-        if (dt <= 0.0) {
-            RCLCPP_WARN(this->get_logger(), "Invalid IMU data time difference: %f", dt);
-            return;
-        }
-        prev_sensor_data_time_ = sensor_now;
+        
         d2_tools::types::IMUdata imu_data;
         imu_data.timestamp = sensor_now;
-        imu_data.acc = Eigen::Vector3d(imu_raw->linear_acceleration.x, imu_raw->linear_acceleration.y, imu_raw->linear_acceleration.z);
-        imu_data.gyro = Eigen::Vector3d(imu_raw->angular_velocity.x, imu_raw->angular_velocity.y, imu_raw->angular_velocity.z);
-        imu_data.is_valid = true; // TODO: validate IMU data
-        Eigen::Vector3d domg = (imu_data.gyro - prev_imu_gyro_) / dt;
+        
+        double acc_scale = is_livox_imu_ ? 9.80665 : 1.0;
+        imu_data.acc = Eigen::Vector3d(
+            imu_raw->linear_acceleration.x * acc_scale,
+            imu_raw->linear_acceleration.y * acc_scale,
+            imu_raw->linear_acceleration.z * acc_scale
+        );
+        imu_data.gyro = Eigen::Vector3d(
+            imu_raw->angular_velocity.x,
+            imu_raw->angular_velocity.y,
+            imu_raw->angular_velocity.z
+        );
+        imu_data.is_valid = true;
+        
+        Eigen::Vector3d domg = Eigen::Vector3d::Zero();
+        
+        if (prev_sensor_data_time_ != 0.0 && prev_imu_gyro_.norm() != 0.0) {
+            double dt = sensor_now - prev_sensor_data_time_;
+            if (dt <= 0.0 || dt > 0.1) {
+                RCLCPP_WARN(this->get_logger(), "Invalid IMU data time difference: %f", dt);
+                prev_sensor_data_time_ = sensor_now;
+                prev_imu_gyro_ = imu_data.gyro;
+                return;
+            }
+            domg = (imu_data.gyro - prev_imu_gyro_) / dt;
+        }
+        
+        prev_sensor_data_time_ = sensor_now;
         prev_imu_gyro_ = imu_data.gyro;
+        
         auto transformed_data = imu_transformer_->transform(domg, imu_data);
+        
         sensor_msgs::msg::Imu imu_transformed;
         imu_transformed.header = imu_raw->header;
         imu_transformed.header.frame_id = output_frame_;
@@ -79,10 +99,12 @@ private:
         imu_transformed.angular_velocity.z = transformed_data.gyro(2);
         imu_transformed.orientation = imu_raw->orientation;
         imu_transformed.orientation_covariance = imu_raw->orientation_covariance;
-        imu_transformed.angular_velocity_covariance = imu_raw->angular_velocity_covariance;
-        imu_transformed.linear_acceleration_covariance = imu_raw->linear_acceleration_covariance;
+        for (size_t i = 0; i < 3; ++i) 
+            imu_transformed.angular_velocity_covariance[i * 3 + i] = 0.01;
+        for (size_t i = 0; i < 3; ++i) 
+            imu_transformed.linear_acceleration_covariance[i * 3 + i] = 1.0;
+        
         imu_pub_->publish(std::move(imu_transformed));
-        // RCLCPP_DEBUG(this->get_logger(), "IMU data transformed and published");
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
@@ -91,6 +113,7 @@ private:
     std::string input_, output_, output_frame_;
     double prev_sensor_data_time_;
     Eigen::Vector3d prev_imu_gyro_;
+    bool is_livox_imu_;
 };
 
 } // namespace d2_tools
